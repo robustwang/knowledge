271c3ad4775074e09d8a01cabceacb235873a6eb
wangjun <robustwang@gmail.com>
2019-07-11 11:19:05 +0800

    #add  sort 2

---
+++ src/main/java/com/robustwang/example/algorithm/Insertion.java
@@ -0,0 +1,29 @@
+package com.robustwang.example.algorithm;
+
+public class Insertion {
+
+    // 插入排序，a 表示数组，n 表示数组大小
+    public void insertionSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        for (int i = 1; i < n; ++i) {
+            int value = a[i];
+            int j = i - 1;
+            // 查找插入的位置
+            for (; j >= 0; --j) {
+                if (a[j] > value) {
+                    a[j+1] = a[j];  // 数据移动
+                } else {
+                    break;
+                }
+            }
+            a[j+1] = value; // 插入数据
+        }
+    }
+
+    public static void main(String[] args) {
+        Insertion insert = new Insertion();
+        int[] a = new int[]{3, 2, 7, 4, 5, 1, 6};
+        insert.insertionSort(a, 7);
+    }
+}
