084821afafea90b714be1e38e70602e22f1c8db4
wangjun <wang.jun@dr-elephant.com>
2019-07-11 11:18:17 +0800

    #add  sort

---
+++ src/main/java/com/robustwang/example/algorithm/Bsearch.java
@@ -0,0 +1,29 @@
+package com.robustwang.example.algorithm;
+
+public class Bsearch {
+
+    public int bsearch(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (a[mid] == value) {
+                return mid;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] bsArray = new int[]{3, 2, 6, 4, 5, 9, 8, 7};
+        Bsearch bs = new Bsearch();
+        int bsearch = bs.bsearch(bsArray, 8, 5);
+        System.out.println(bsearch);
+    }
+}
