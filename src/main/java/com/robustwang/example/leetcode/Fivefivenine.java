6cb3c90d597dc47dfee69208b3a1b632adc0436f
wangjun <wang.jun@dr-elephant.com>
2020-04-19 22:36:33 +0800

    #leetcode

---
+++ src/main/java/com/robustwang/example/leetcode/Fivefivenine.java
@@ -0,0 +1,40 @@
+package com.robustwang.example.leetcode;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Fivefivenine {
+
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        } else if (root.children.isEmpty()) {
+            return 1;
+        } else {
+            List<Integer> heights = new LinkedList<>();
+            for (Node item : root.children) {
+                heights.add(maxDepth(item));
+            }
+            return Collections.max(heights) + 1;
+        }
+    }
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+    ;
+}
