8a2b29e094347119017fee94bf08aed7a3ea06e7
wangjun <wang.jun@dr-elephant.com>
2020-05-12 10:55:27 +0800

    #add some code for cache

--- src/main/java/com/robustwang/example/core/LRUCache2.java
+++ src/main/java/com/robustwang/example/core/LRUCache2.java
@@ -10,39 +10,25 @@ public class LRUCache2 {
     }
 
     private void addNode(DLinkedNode node) {
-        /**
-         * Always add the new node right after head.
-         */
         node.prev = head;
         node.next = head.next;
-
         head.next.prev = node;
         head.next = node;
     }
 
     private void removeNode(DLinkedNode node){
-        /**
-         * Remove an existing node from the linked list.
-         */
         DLinkedNode prev = node.prev;
         DLinkedNode next = node.next;
-
         prev.next = next;
         next.prev = prev;
     }
 
     private void moveToHead(DLinkedNode node){
-        /**
-         * Move certain node in between to the head.
-         */
         removeNode(node);
         addNode(node);
     }
 
     private DLinkedNode popTail() {
-        /**
-         * Pop the current tail.
-         */
         DLinkedNode res = tail.prev;
         removeNode(res);
         return res;
