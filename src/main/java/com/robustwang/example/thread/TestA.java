ef16bc5296555e55f6ae620e22a0d7ace6d48d07
wangjun <wang.jun@dr-elephant.com>
2020-07-28 18:02:04 +0800

    12345678

--- src/main/java/com/robustwang/example/thread/TestA.java
+++ src/main/java/com/robustwang/example/thread/TestA.java
@@ -2,6 +2,8 @@ package com.robustwang.example.thread;
 
 public class TestA {
     public static void main(String[] args) {
-
+        System.out.println("hello word");
+        System.out.println("123");
+        System.out.println("ok");
     }
 }
