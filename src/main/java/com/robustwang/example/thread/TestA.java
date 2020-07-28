69b04721c8d2800653848dc547734dd1251e3ac8
wangjun <wang.jun@dr-elephant.com>
2020-07-28 17:57:33 +0800

    Revert "add findDuplicate"

--- src/main/java/com/robustwang/example/thread/TestA.java
+++ src/main/java/com/robustwang/example/thread/TestA.java
@@ -3,5 +3,6 @@ package com.robustwang.example.thread;
 public class TestA {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("123");
     }
 }
