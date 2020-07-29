48127405e1352a48950b32e1276bdb6e5f794772
wangjun <wang.jun@dr-elephant.com>
2020-07-29 12:46:46 +0800

    test:wq :

--- src/main/java/com/robustwang/example/thread/TestA.java
+++ src/main/java/com/robustwang/example/thread/TestA.java
@@ -2,6 +2,11 @@ package com.robustwang.example.thread;
 
 public class TestA {
     public static void main(String[] args) {
+        System.out.println("2");
 
+
+        System.out.println("3");
+
+        System.out.println("4");
     }
 }
