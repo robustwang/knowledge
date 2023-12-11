0a58753d8bc2fc355210f3d4e4d1581024d5209e
wangjun <wang.jun@dr-elephant.com>
2023-12-11 20:22:28 +0800

    add isPalindrome test case

---
+++ src/main/java/com/robustwang/example/algorithm/IsPalindrome.java
@@ -0,0 +1,32 @@
+package com.robustwang.example.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsPalindrome {
+
+    public boolean isPalindrome(ListNode head) {
+
+        List<Integer> vals = new ArrayList<>();
+
+        // 将链表的值复制到数组中
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // 使用双指针判断是否回文
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+
+}
